f02155a8ac42e98f6a30a7029389143d3c87fd42
carry chen <carrychen999@outlook.com>
2025-08-05 09:13:21 +0800

    修改

--- src/main/java/com/carryon/disrupter/disrupter/OrderEventHandler.java
+++ src/main/java/com/carryon/disrupter/disrupter/OrderEventHandler.java
@@ -6,7 +6,7 @@ public class OrderEventHandler implements EventHandler<OrderEvent> {
 
     public void onEvent(OrderEvent event, long sequence, boolean endOfBatch) throws Exception {
         Thread.sleep(1); // 模拟处理时间，注释掉原来会卡住的代码
-        System.err.println("消费者: " + event.getValue());
+        System.out.println("消费者: " + event.getValue());
     }
 
 }
